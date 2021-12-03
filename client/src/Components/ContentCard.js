da410685ee95a3e3c1c3b891029c48a8251fd102
himanshu007-creator <addyjeridiq@gmail.com>
2021-12-03 23:36:58 +0530

    initial commit

--- client/src/Components/ContentCard.js
+++ client/src/Components/ContentCard.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Card } from "react-bootstrap";
 
 const ContentCard = (props) => {
+  const visible = { width: "18rem", margin: 10 };
+  const invisible = { width: "18rem", margin: 10, visibility: "hidden" };
+  const stl = props.show === "true" ? visible : invisible;
   return (
-    <Card style={{ width: "18rem", margin: 10 }} className="shadow-lg">
+    <Card style={stl} className="shadow-lg">
       <Card.Body className="d-flex flex-column justify-content-between">
         <div>
           <Card.Title>{props.title}</Card.Title>
